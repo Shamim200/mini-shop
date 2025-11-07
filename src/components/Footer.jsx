33635e7b87d9813b90fe60674961bc1882047326
shamim 200 <s40644812@gmail.com>
2025-11-07 22:21:03 +0600

    ✨ feat(layout): implement basic footer component
    - add footer component with basic layout and styling
    - include a container and centered text for the footer content

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Container fluid className="bg-primary p-4">
+      <p className="text-center">footer</p>
+    </Container>
+  );
+};
+export default Footer;
