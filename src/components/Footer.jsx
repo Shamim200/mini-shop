5d6ef7139e1be3595cc94c7bee1342a9bac9ce9f
shamim 200 <s40644812@gmail.com>
2025-12-04 22:27:29 +0600

    💄 style(footer): update footer style and content - change background color to secondary - update text to copyright information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <Container fluid className="bg-primary p-4">
-      <p className="text-center">footer</p>
+    <Container fluid className="bg-secondary p-4">
+      <p className="text-center text-capitalize">
+        &copy; copyright | all right resered at shamim200
+      </p>
     </Container>
   );
 };
