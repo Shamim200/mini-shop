d8687fd4eadccb3058c312ca75c383e83a67ec56
shamim 200 <s40644812@gmail.com>
2025-12-04 22:27:41 +0600

    ✨ feat(header): implement shopping cart functionality
    - add cart icon with badge to display number of items
    - show "your cart is empty" message when cart is empty

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,27 +2,27 @@ import { Navbar, Nav, Container, Dropdown, Badge } from "react-bootstrap";
 import { Link, NavLink } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 import DarkMode from "./DarkMode";
+import { useSelector } from "react-redux";
 
 const navList = [
   {
     title: "home",
     links: "/",
   },
-  {
-    title: "about",
-    links: "/about",
-  },
+
   {
     title: "products",
     links: "/products",
   },
   {
-    title: "contact",
-    links: "/contact",
+    title: "categories",
+    links: "/categories",
   },
 ];
 
 const Header = () => {
+  const cart = useSelector((state) => state.cart.items);
+
   return (
     <Navbar
       sticky="top"
@@ -56,11 +56,12 @@ const Header = () => {
             <Dropdown align="end">
               <Dropdown.Toggle variant="success">
                 <FaShoppingCart color="white" fontSize="15px" />
-                <Badge>0</Badge>
+                {/* <Badge>0</Badge> */}
+                <Badge>{cart.length}</Badge>
               </Dropdown.Toggle>
 
               <Dropdown.Menu style={{ minWidth: 370 }}>
-                <p>Your Cart is Empty!</p>
+                {cart.length === 0 && <p>your cart is empty</p>}
               </Dropdown.Menu>
             </Dropdown>
           </Nav>
