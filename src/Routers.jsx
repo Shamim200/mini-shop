6a664cd5106a63bb5fce7cce389907544e8f9cd3
shamim 200 <s40644812@gmail.com>
2025-12-04 22:31:16 +0600

    ♻️ refactor(router): update routes and remove unused components
    - remove About and Contact routes and components
    - add Categories route and component

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -3,23 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import Loader from "./components/Loader";
 
 const Home = lazy(() => import("./pages/Home"));
-const About = lazy(() => import("./pages/About"));
 const Products = lazy(() => import("./pages/Products"));
-const Contact = lazy(() => import("./pages/Contact"));
 const Signup = lazy(() => import("./pages/Signup"));
 const Signin = lazy(() => import("./pages/Signin"));
 const Cart = lazy(() => import("./pages/Cart"));
 const NotFound = lazy(() => import("./pages/NotFound"));
+const Categories = lazy(() => import("./pages/Categories"));
 const Routers = () => {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-
         <Route path="/products" element={<Products />} />
-
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/cart" element={<Cart />} />
