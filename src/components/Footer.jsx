74ff4d460757f945e8dc9d2c9da98918452d2204
shamim 200 <s40644812@gmail.com>
2025-12-10 11:57:29 +0600

    🐛 fix(footer): update footer background color to dark and ensure text is white

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <Container fluid className="bg-secondary p-4">
+    <Container fluid className="bg-dark text-white p-4">
       <p className="text-center text-capitalize">
         &copy; copyright | all right resered at shamim200
       </p>
