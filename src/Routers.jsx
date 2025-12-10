4173e0c10bf7ff8920c125af233be7c28fb2c255
shamim 200 <s40644812@gmail.com>
2025-12-11 00:32:18 +0600

    feat(routes): ✨add route for product details page

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -4,17 +4,20 @@ import Loader from "./components/Loader";
 
 const Home = lazy(() => import("./pages/Home"));
 const Products = lazy(() => import("./pages/Products"));
+const SingelProduct = lazy(() => import("./pages/SingelProduct"));
 const Signup = lazy(() => import("./pages/Signup"));
 const Signin = lazy(() => import("./pages/Signin"));
 const Cart = lazy(() => import("./pages/Cart"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 const Categories = lazy(() => import("./pages/Categories"));
+
 const Routers = () => {
   return (
     <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SingelProduct />} />
         <Route path="/categories" element={<Categories />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
