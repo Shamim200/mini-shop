7650aac501ae4c06439179b54ec6e5e8d8044d43
shamim 200 <s40644812@gmail.com>
2025-12-12 15:16:07 +0600

    feat(home): ✨update Home component to display service features in a grid layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,51 @@
-import { Container } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
+import { FaTruck } from "react-icons/fa";
+import { RiRefund2Line } from "react-icons/ri";
+import { TbDiscountFilled } from "react-icons/tb";
+import { MdSupportAgent } from "react-icons/md";
+
+const data = [
+  {
+    icon: <FaTruck />,
+    title: "Free Delivery",
+    desc: "Orders from all items",
+  },
+  {
+    icon: <RiRefund2Line />,
+    title: "Return & Refund",
+    desc: "Money back guarantee",
+  },
+  {
+    icon: <TbDiscountFilled />,
+    title: "Member Discount",
+    desc: "On order over $99",
+  },
+  {
+    icon: <MdSupportAgent />,
+    title: "Support 24/7",
+    desc: "Contact us 24 hours a day",
+  },
+];
 
 const Home = () => {
   return (
-    <Container>
-      <div className="container">
-        <h4>Home Page</h4>
-      </div>
-    </Container>
+    <Row className="g-3 p-4">
+      {data.map((item) => {
+        return (
+          <Col lg={3} md={6} sm={12} key={item.title}>
+            <div className="my-grid">
+              <div style={{ fontSize: "2rem" }}>{item.icon}</div>
+              <div>
+                <p>
+                  {item.title} <br />
+                  {item.desc}
+                </p>
+              </div>
+            </div>
+          </Col>
+        );
+      })}
+    </Row>
   );
 };
 export default Home;
