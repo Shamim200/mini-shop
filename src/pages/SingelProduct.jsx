d14d79a31f55852dfa21d0587240d30f4c67a222
shamim 200 <s40644812@gmail.com>
2025-12-18 14:08:33 +0600

    feat(product): ✨enhance SingleProduct layout with image and details

--- src/pages/SingelProduct.jsx
+++ src/pages/SingelProduct.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useGetProudctByIdQuery } from "../services/api";
 import Loader from "../components/Loader";
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 const SingelProduct = () => {
   const { id } = useParams();
@@ -14,7 +14,35 @@ const SingelProduct = () => {
 
   return (
     <Container className="my-5">
-      <h4>{product.title}</h4>
+      <div className="d-flex justify-content-start align-items-start">
+        <img className="w-50" src={product.thumbnail} alt={product.title} />
+        <div>
+          <h3>{product.title}</h3>
+          <p>{product.description}</p>
+          <p>category: {product.category}</p>
+          <p>brand: {product.brand}</p>
+          <p>weight: {product.weight}kg</p>
+
+          <div className="my-3 d-flex justify-content-center flex-column gap-4">
+            {product.reviews.map((item) => {
+              return (
+                <div className="bg-secondary p-4 rounded" key={item.rating}>
+                  <h4>{item.reviewerName}</h4>
+                  <p>Reviewed on: {new Date(item.date).toLocaleDateString()}</p>
+                  <p>{item.reviewerEmail}</p>
+                  <p>Rating: {item.rating}</p>
+                  <p>{item.comment}</p>
+                </div>
+              );
+            })}
+          </div>
+
+          <p>price: {product.price}</p>
+          <Button variant="outline-primary" className="text-capitalize">
+            add to cart
+          </Button>
+        </div>
+      </div>
     </Container>
   );
 };
