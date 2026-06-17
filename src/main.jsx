9dfd73360d9d6c5bc28afa812128f6e69952b5bf
Shamim 200 <s40644812@gmail.com>
2026-06-17 22:37:43 +0600

    ✨feat(client/router): add new routing point & remove old routing point

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,65 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import Products from "./pages/Products.jsx";
+import Categories from "./pages/Categories.jsx";
+import Signup from "./pages/Signup.jsx";
+import Signin from "./pages/Signin.jsx";
+import Cart from "./pages/Cart.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import SingelProduct from "./pages/SingelProduct.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    // loader: true,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/products/:id",
+        element: <SingelProduct />,
+      },
+      {
+        path: "/categories",
+        element: <Categories />,
+        errorElement: (
+          <div className="my-5 text-center p-4 text-capitalize">
+            something went wrong!
+          </div>
+        ),
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      {
+        path: "/signin",
+        element: <Signin />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
-  </Provider>
+    <RouterProvider router={router} />
+  </Provider>,
 );
