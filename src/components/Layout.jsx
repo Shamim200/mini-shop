5c600e32c9bd7e0df6c65b8efeee0e20e32628a0
Shamim 200 <s40644812@gmail.com>
2026-06-17 22:38:29 +0600

    ✨feat(client/layout): add outlet to children prop

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import Footer from "../layouts/Footer";
+import Header from "../layouts/Header";
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
