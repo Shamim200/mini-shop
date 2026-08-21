ae661be4d6d7f940fc4abc809d2d7c438ea88e03
Shamim 200 <s40644812@gmail.com>
2026-08-21 22:22:10 +0600

    ✨ feat(product): enhance SingleProduct component with improved layout and review display

--- src/pages/SingelProduct.jsx
+++ src/pages/SingelProduct.jsx
@@ -2,8 +2,11 @@ import { useParams } from "react-router-dom";
 import { useGetProudctByIdQuery } from "../services/api";
 import Loader from "../components/Loader";
 import { Button, Col, Container, Row } from "react-bootstrap";
+import useCart from "../Hooks/useCart";
 
 const SingelProduct = () => {
+  const { isInCart, handelAddToCart, handelRemoveFromCart } = useCart();
+
   const { id } = useParams();
   const { data: product, isLoading } = useGetProudctByIdQuery(id);
   if (isLoading) {
@@ -12,40 +15,71 @@ const SingelProduct = () => {
 
   if (!product) return <div>No product found!</div>;
 
+  const {
+    thumbnail,
+    title,
+    description,
+    category,
+    brand,
+    weight,
+    reviews,
+    price,
+  } = product;
+
   return (
     <Container>
-      <div className="d-flex m-3 flex-column flex-md-row justify-content-md-start align-items-md-start gap-4">
+      <div className="p-4">
         <img
-          className="img-fluid"
-          src={product.thumbnail}
-          alt={product.title}
+          src={thumbnail}
+          className="d-block img-fluid mx-auto"
+          alt={title}
         />
-        <div className="flex-wrap">
-          <h3>{product.title}</h3>
-          <p>{product.description}</p>
-          <p>category: {product.category}</p>
-          <p>brand: {product.brand}</p>
-          <p>weight: {product.weight}kg</p>
-
-          <div className="my-3 d-flex justify-content-center flex-column gap-4">
-            {product.reviews.map((item) => {
-              return (
-                <div className="bg-secondary p-4 rounded" key={item.rating}>
-                  <h4>{item.reviewerName}</h4>
-                  <p>Reviewed on: {new Date(item.date).toLocaleDateString()}</p>
-                  <p>{item.reviewerEmail}</p>
-                  <p>Rating: {item.rating}</p>
-                  <p>{item.comment}</p>
-                </div>
-              );
-            })}
-          </div>
-
-          <p>price: {product.price}</p>
-          <Button variant="outline-primary" className="text-capitalize">
-            add to cart
-          </Button>
+        <div className="">
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <p>category: {category}</p>
+          <p>brand: {brand}</p>
+          <p>weight: {weight}kg</p>
         </div>
+        <Row className="g-3 my-2">
+          {reviews.map((item) => {
+            const { reviewerName, date, reviewerEmail, rating, comment } = item;
+            return (
+              <Col key={rating} lg={4} md={6} sm={12}>
+                <div className="border p-4 rounded singel-products">
+                  <h4>{reviewerName}</h4>
+                  <p>{new Date(date).toLocaleDateString()}</p>
+                  <p>{reviewerEmail}</p>
+                  <p>Rating: {rating}</p>
+                  <p>{comment}</p>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+
+        {isInCart(product.id) ? (
+          <Button
+            variant="danger"
+            onClick={() => handelRemoveFromCart({ id: product.id })}
+          >
+            remove
+          </Button>
+        ) : (
+          <Button
+            variant="primary"
+            onClick={() =>
+              handelAddToCart({
+                id: product.id,
+                title,
+                price,
+                thumbnail,
+              })
+            }
+          >
+            add
+          </Button>
+        )}
       </div>
     </Container>
   );
