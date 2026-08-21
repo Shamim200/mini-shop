45d2c1e19710418659a87a42c1086cbae87c9cb2
Shamim 200 <s40644812@gmail.com>
2026-08-21 22:16:34 +0600

    ✨ feat(cart): implement custom hook for cart management

---
+++ src/Hooks/useCart.jsx
@@ -0,0 +1,56 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  addToCart,
+  removeCart,
+  clearCart,
+  incrementQuantity,
+  decrementQuantity,
+} from "../features/cart/cart";
+
+const useCart = () => {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.items);
+
+  const isInCart = (id) => {
+    return cart.some((item) => item.id === id);
+  };
+
+  // handel add to cart
+  const handelAddToCart = ({ id, title, price, thumbnail }) => {
+    dispatch(addToCart({ id, title, price, thumbnail }));
+  };
+
+  // handel remove from cart
+  const handelRemoveFromCart = ({ id }) => {
+    dispatch(removeCart(id));
+  };
+  // total amount
+  const totalAmount = cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0,
+  );
+  // all remove from cart
+  const handelClearCart = () => {
+    dispatch(clearCart());
+  };
+
+  const handelIncrementQuantity = () => {
+    dispatch(incrementQuantity());
+  };
+  const handelDecrementQuantity = () => {
+    dispatch(decrementQuantity());
+  };
+
+  return {
+    isInCart,
+    handelAddToCart,
+    handelRemoveFromCart,
+    handelClearCart,
+    handelIncrementQuantity,
+    handelDecrementQuantity,
+    totalAmount,
+  };
+};
+
+export default useCart;
