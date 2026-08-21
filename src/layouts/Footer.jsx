ba1e13e11472f2e37298c167908a159698646644
Shamim 200 <s40644812@gmail.com>
2026-08-21 22:23:20 +0600

    ✨ feat(footer): add Footer component for displaying copyright information

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <Container fluid className="bg-dark text-white p-4">
+      <p className="text-center text-capitalize">
+        &copy; copyright | all right resered at shamim200
+      </p>
+    </Container>
+  );
+};
+export default Footer;
